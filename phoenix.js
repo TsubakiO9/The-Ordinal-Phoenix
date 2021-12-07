0e91784ede2aea952fb028eed6b75ca47d32a557
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-07 23:10:25 +0100

    initial load

--- phoenix.js
+++ phoenix.js
@@ -5,7 +5,6 @@ import phoenixItemSheet from "./module/item/sheet.js";
 Hooks.once("init", function () {
     console.log("phoenix | Initialising The Ordinal Adventure System");
 
-    CONFIG.PHOENIX = PHOENIX;
 
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("phoenix", phoenixItemSheet, { makeDefault: true });
