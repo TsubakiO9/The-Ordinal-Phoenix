72908ebfc553b135212c25b2383e9470dd0d8fa1
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-07 20:50:43 +0100

    initial load

--- phoenix.js
+++ phoenix.js
@@ -1,12 +1,20 @@
 import { PHOENIX } from "./module/config.js";
-import { ItemSheet } from "./module/item/sheet.js";
-import {} from "";
 
+// Importar Documentos
+import ActorPhoenix from "./module/actor/entity.js";
+import ItemPhoenix from "./module/item/entity.js";
+import { TokenDocumentPhoenix, TokenPhoenix } from "./module/token.js";
+
+// Configuración Inicial de los Valores
+CONFIG.PHOENIX = PHOENIX;
+CONFIG.time.roundTime = 10;
+
+// Hooks
 Hooks.once("init", function () {
     console.log("PHOENIX | Initialising Ordinal Adventure");
     
     CONFIG.PHOENIX = PHOENIX;
 
-    Items.unregisterSheet("core", ItemSheet);
-    Items.registerSheet("PHOENIX", ItemSheet, { makeDefault: true });
+    Items.unregisterSheet("core", ItemPhoenix);
+    Items.registerSheet("PHOENIX", ItemPhoenix, { makeDefault: true });
 });
