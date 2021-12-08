f06248c7b3908ef6fa88d051e3baefd86f33c7fa
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-08 18:07:42 +0100

    initial load

--- phoenix.js
+++ phoenix.js
@@ -1,27 +1,51 @@
-// Import Modules
+// Import Modules:
 import {PHOENIX} from "./module/config.js";
 
 
-// Import Documents
+// Import Documents:
 
 
-// Import Applications
+// Import Applications:
 import ActorSheetPhoenixPNJ from "./module/actor/sheets/.js";
 import ActorSheetPhoenixNPC from "./module/actor/sheets/.js";
 import ActorSheetPhoenixVehicle from "./module/actor/sheets/v.js";
 import ItemSheetPhoenix from "./module/item/sheet.js";
 
 
-// Hooks
-Hooks.once("init", function () {
-    console.log("phoenix | Initialising The Ordinal Adventure System");
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+Hooks.once("init", function() {
+    console.log(`DnD5e | Initializing the DnD5e Game System\n${DND5E.ASCII}`);
+
+// Registro de plantillas:
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("phoenix", ActorSheetPhoenixPNJ, {
+        types: ["PNJ"],
+        makeDefault: true,
+        label: "PHOENIX.SheetClassPNJ"
+    });
+
+    Actors.registerSheet("phoenix", ActorSheetPhoenixNPC, {
+        types: ["npc"],
+        makeDefault: true,
+        label: "DND5E.SheetClassNPC"
+    });
 
     Items.unregisterSheet("core", ItemSheet);
-    Items.registerSheet("phoenix", ItemSheetPhoenix, { makeDefault: true });
+    Items.registerSheet("dnd5e", ItemSheet5e, {
+        makeDefault: true,
+        label: "DND5E.SheetClassItem"
+    });
+
+
+// Preload Handlebars Templates
+    return preloadHandlebarsTemplates();
 });
 
 
- // Record Configuration Values
+ // Record Configuration Values:
  CONFIG.DND5E = DND5E;
  CONFIG.Actor.documentClass = ActorPhoenix;
  CONFIG.Item.documentClass = ItemPhoenix;
