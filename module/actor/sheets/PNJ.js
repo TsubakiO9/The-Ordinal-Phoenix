c02870c9c96d61666611e293d1035ce822871730
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-08 20:05:39 +0100

    initial load

--- module/actor/sheets/PNJ.js
+++ module/actor/sheets/PNJ.js
@@ -3,7 +3,7 @@ import ActorSheetPhoenix from "./base.js";
 export default class ActorSheetPhoenixPNJ extends ActorSheetPhoenix {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
-          classes: ["phoenix", "sheet", "actor", "pnj"],
+          classes: ["phoenix", "sheet", "actor", "PNJ"],
           width: 720,
           height: 680
         });
