218e7b6a677d3bcd94624cc1ac2a5c8c4d7cc07f
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-08 22:45:53 +0100

    initial load

---
+++ module/helpers/config.js
@@ -0,0 +1,23 @@
+export const PHOENIX = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ PHOENIX.abilities = {
+  "str": "PHOENIX.AbilityStr",
+  "dex": "PHOENIX.AbilityDex",
+  "con": "PHOENIX.AbilityCon",
+  "int": "PHOENIX.AbilityInt",
+  "wis": "PHOENIX.AbilityWis",
+  "cha": "PHOENIX.AbilityCha"
+};
+
+PHOENIX.abilityAbbreviations = {
+  "str": "PHOENIX.AbilityStrAbbr",
+  "dex": "PHOENIX.AbilityDexAbbr",
+  "con": "PHOENIX.AbilityConAbbr",
+  "int": "PHOENIX.AbilityIntAbbr",
+  "wis": "PHOENIX.AbilityWisAbbr",
+  "cha": "PHOENIX.AbilityChaAbbr"
+};
