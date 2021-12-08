f79a852689cb11e903ea00c0f450f959f5ee05b1
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-08 17:26:21 +0100

    initial load

--- module/config.js
+++ module/config.js
@@ -12,25 +12,38 @@ PHOENIX.Atributos = {
 PHOENIX.Caracteristicas = {
     pa: "PHOENIX.CaracteristicaPa",
     pm: "PHOENIX.CaracteristicaPm",
-    curacion: "PHOENIX.CaracteristicaCuracion",
+
+    curación: "PHOENIX.CaracteristicaCuracion",
     muerte: "PHOENIX.CaracteristicaMuerte",
+
     iniciativa: "PHOENIX.CaracteristicaIniciativa",
     velocidad: "PHOENIX.CaracteristicaVelocidad",
+
     mele: "PHOENIX.CaracteristicaMele",
     rango: "PHOENIX.CaracteristicaRango",
     magia: "PHOENIX.CaracteristicaMagia",
     defensa: "PHOENIX.CaracteristicaDefensa",
+
     bloquear: "PHOENIX.CaracteristicaBloquear",
     evadir: "PHOENIX.CaracteristicaEvadir"
 }
 
 PHOENIX.Dominios = {
-    fisico: "PHOENIX.Dominio",
-    social: "PHOENIX.Dominio",
-    subterfujio: "PHOENIX.Dominio",
-    intelectual: "PHOENIX.Dominio",
-    creativo: "PHOENIX.Dominio",
-    perceptivo: "PHOENIX.Dominio"
+    fisico: "PHOENIX.DominioFisico",
+    social: "PHOENIX.DominioSocial",
+    subterfujio: "PHOENIX.DominioSubterfujio",
+    intelectual: "PHOENIX.DominioIntelectual",
+    creativo: "PHOENIX.DominioCreativo",
+    perceptivo: "PHOENIX.DominioPerceptivo"
+}
+
+PHOENIX.Resistencias = {
+    frio: "PHOENIX.ResistenciaFrio",
+    calor: "PHOENIX.ResistenciaCalor",
+    electrica: "PHOENIX.ResistenciaElectrica",
+    emfermedad: "PHOENIX.ResistenciaEmfermedad",
+    fisica: "PHOENIX.Resistenciafisica",
+    veneno: "PHOENIX.ResistenciaVeneno"
 }
 
 PHOENIX.attackTypes = {
