aead9a311b8381c9fdab89ec0a776a3d4702c676
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2021-12-08 21:47:43 +0100

    initial load

--- module/actor/sheets/base.js
+++
@@ -1,3 +0,0 @@
-export default class ActorSheetPhoenix extends ActorSheet {
-    
-};
--- module/item/sheet.js
+++
@@ -1,6 +0,0 @@
-export default class ItemSheetPhoenix extends ItemSheet {
-    get template() {
-        const path = "systems/phoenix/templates/items/";
-        return `${path}/${this.item.data.type}-.html`;
-    }
-}
